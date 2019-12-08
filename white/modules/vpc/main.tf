631c8c95840e251522ec5221203e82dbaa9ed06d
white <white@nani.ws>
2019-12-08 16:55:59 +0100

    restructured filestructure

--- white/modules/vpc/main.tf
+++ white/modules/vpc/main.tf
@@ -1,10 +1,14 @@
 # Configure the AWS Provider
 provider "aws" {
   version = "~> 2.0"
-  region  = "us-east-1"
+  region  = var.region
 }
 
 # Create a VPC
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = var.name
+  }
 }
---
+++ white/modules/vpc/vars.tf
@@ -0,0 +1,14 @@
+variable "cidr" {
+    description = "CIDR Definition for VPC"
+    type = "string"
+}
+
+variable "region" {
+    description = "AWS Region"
+    type = "srting"
+}
+
+variable "name" {
+    description = "VPC Name"
+    type = "string"
+}
