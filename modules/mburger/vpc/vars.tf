12cdd08accbd6216b1a4fd6594c861f973c93308
Markus Burger <markus.burger@jumio.com>
2019-12-10 21:22:15 +0100

    checkpoint, done vpc definition

--- modules/mburger/vpc/vars.tf
+++ modules/mburger/vpc/vars.tf
@@ -12,3 +12,18 @@ variable "name" {
   description = "the name of the env"
   type = string
 }
+
+variable "availability_zones" {
+  description = "the azs that the subnets will be spawned in"
+  type = list(string)
+}
+
+variable "public_subnet_cidr" {
+  description = "the subnet cidrs of the public subnets"
+  type = list(string)
+}
+
+variable "private_subnet_cidr" {
+  description = "the subnet cidrs of the private subnets"
+  type = list(string)
+}
